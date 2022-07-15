db41794658cceda370e5e4922661509aaf0a2ee3
IgorBorgesDaMata <igorbdamata@gmail.com>
2022-07-15 18:30:09 -0300

    Game over e vida do player

---
+++ JogoGMTK2022/Assets/Scripts/Divers/CameraShakeController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraShakeController : MonoBehaviour
+{
+    public void SetShake(AnimationCurve gain, AnimationCurve freuquency, float shakeTime)
+    {
+        print("Chamou");
+        StartCoroutine(SetShakeIEnumerator(gain, freuquency, shakeTime));
+    }
+
+    private IEnumerator SetShakeIEnumerator(AnimationCurve gain, AnimationCurve frequency, float shakeTime)
+    {
+        CinemachineBasicMultiChannelPerlin perlin = GetComponent<CinemachineVirtualCamera>().
+                                                    GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+        float timer = 0;
+        while (timer < shakeTime)
+        {
+
+            perlin.m_AmplitudeGain = gain.Evaluate(timer);
+            perlin.m_FrequencyGain = frequency.Evaluate(timer);
+            timer += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+
+        perlin.m_AmplitudeGain = 0;
+        perlin.m_FrequencyGain = 0;
+    }
+}
--- JogoGMTK2022/Assets/Scripts/UI/UI.cs
+++ JogoGMTK2022/Assets/Scripts/UI/UI.cs
@@ -10,9 +10,28 @@ using System.Linq;
 public class UI : MonoBehaviour
 {
     public static UI ui;
+    [SerializeField] private GameObject loadingScreenPrefab;
+    [SerializeField] private GameObject gameOverScreen;
+
     private void Awake()
     {
         if (ui == null) { ui = this; return; }
         Destroy(gameObject);
+    }
+
+    public void RetryLevel() { LoadScene(GameController.gc.currentScene); }
+
+    public void LoadScene(int index)
+    {
+        SaveSystem.SaveInt("SceneToLoad", index);
+        Instantiate(loadingScreenPrefab, transform.position, transform.rotation);
+    }
+    public void Quit() { Application.Quit(); }
+
+    public void SetGameOverScreen()
+    {
+        gameOverScreen.SetActive(true);
+        gameOverScreen.transform.GetChild(2).GetComponent<TextMeshProUGUI>().text="Score: " + GameController.gc.playerScore;
+        gameOverScreen.transform.GetChild(3).GetComponent<TextMeshProUGUI>().text="High score: " + DATA.d.playerHighScore;
     }
 }
